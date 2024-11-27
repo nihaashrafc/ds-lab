a341b7b2a5c21221746872510eda8def466864f8
nihaashrafc <nihaashh@gmail.com>
2024-11-27 14:28:27 +0530

    Add files via upload

---
+++ mergearray.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+void main()
+{
+int m,n,a[10],b[10],c[20],i,j,t,k=0;
+printf("enter size of array a:");
+scanf("%d",&m);
+printf("enter array elements:");
+for(i=0;i<m;i++)
+scanf("%d",&a[i]);
+printf("enter the size of array b:");
+scanf("%d",&n);
+printf("enter array elements:");
+for(i=0;i<n;i++)
+scanf("%d",&b[i]);
+for(i=0;i<m;i++)
+{
+for(j=i+1;j<m;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+for(j=i+1;j<n;j++)
+if(b[i]>b[j])
+{
+t=b[i];
+b[i]=b[j];
+b[j]=t;
+}
+i=j=0;
+while(i<m && j<n)
+{
+if(a[i]<=b[j])
+{
+c[k]=a[i];
+i++;
+k++;
+}
+else{
+c[k]=b[j];
+j++;
+k++;
+}
+}
+while(i<m)
+{
+c[k]=a[i];
+i++;
+k++;
+}
+while(j<n)
+{
+c[k]=b[j];
+j++;
+k++;
+}
+printf("\n array a:");
+for(i=0;i<m;i++)
+printf("%d\t",a[i]);
+printf("\n array b:");
+for(i=0;i<n;i++)
+printf("%d\t",b[i]);
+printf("\n merged array c:");
+for(i=0;i<m+n;i++)
+printf("%d\t",c[i]);
+}
+
+
+
+
+
+
